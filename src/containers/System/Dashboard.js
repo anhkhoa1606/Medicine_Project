7bc07a35ebfefb7140ea84c2beefa4880405a2b1
anhkhoa@gmail.com <anhkhoa@gmail.com>
2025-03-18 19:09:49 +0700

    fix dashboard

--- src/containers/System/Dashboard.js
+++ src/containers/System/Dashboard.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Home, Video, List, Clock, ThumbsUp, Scissors } from "lucide-react"; // Dùng icon từ lucide-react
 import "./Dashboard.scss"; // File CSS để tạo kiểu
 import { FormattedMessage } from "react-intl";
+import { Users, Package, Tags, ShoppingCart, BarChart2 } from "lucide-react";
 
 export const adminMenu = [
-  {
-    name: "menu.system.header",
-    menus: [
-      { name: "menu.system.system-administrator.user-manage", link: "/system/user-manage", icon: <Home size={20} /> },
-      { name: "menu.system.system-administrator.product-manage", link: "/system/product-manage", icon: <Scissors size={20} /> },
-      { name: "menu.system.system-administrator.category-manage", link: "/system/category-manage", icon: <Video size={20} /> },
-      { name: "menu.system.system-administrator.order-manage", link: "/system/order-manage", icon: <Clock size={20} /> },
-      { name: "menu.system.system-administrator.dashboard", link: "/system/dashboard", icon: <List size={20} /> },
-    ],
-  },
-];
+    {
+      name: "menu.system.header",
+      menus: [
+        { name: "menu.system.system-administrator.user-manage", link: "/system/user-manage", icon: <Users size={20} /> },
+        { name: "menu.system.system-administrator.product-manage", link: "/system/product-manage", icon: <Package size={20} /> },
+        { name: "menu.system.system-administrator.category-manage", link: "/system/category-manage", icon: <Tags size={20} /> },
+        { name: "menu.system.system-administrator.order-manage", link: "/system/order-manage", icon: <ShoppingCart size={20} /> },
+        { name: "menu.system.system-administrator.revenue", link: "/system/revenue", icon: <BarChart2 size={20} /> },
+      ],
+    },
+  ];
 
 class DashboardSidebar extends Component {
   render() {

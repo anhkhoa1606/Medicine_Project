c0de8b77191d3b046cd4ef17b6cd124cd8b67c0c
anhkhoa@gmail.com <anhkhoa@gmail.com>
2025-04-25 18:35:28 +0700

    fix phoneNumber

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -13,7 +13,7 @@ let createNewUser = async (data) => {
                 firstName: data.firstName,
                 lastName: data.lastName,
                 address: data.address,
-                phonenumber: data.phonenumber,
+                phoneNumber: data.phoneNumber,
                 gender: data.gender === '1' ? true : false,
                 roleId: data.roleId,
             })
